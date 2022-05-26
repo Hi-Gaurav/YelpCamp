730642ffca736d7488841612e8ba1bc467cb9ac2
Hi-Gaurav <hi.kshatriya@gmail.com>
2022-05-26 22:20:08 +0530

    added user authorization

--- app.js
+++ app.js
@@ -2,56 +2,55 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const methodOverride = require('method-override');
+const session = require('express-session');
+const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const { cookie } = require('express/lib/response');
 const { campgroundSchema, reviewSchema } = require('./schemas.js');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
-const methodOverride = require('method-override');
 const Campground = require('./models/campground');
-const session = require('express-session')
-const flash = require('connect-flash')
 const Review = require('./models/review');
-const passport = require('passport');
-const LocalStrategy = require('passport-local')
-const User = require('./models/user')
+const User = require('./models/user');
 
 const userRoutes = require('./routes/users');
 // const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
-const campgrounds = require('./routes/campgrounds')
+const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
-const { cookie } = require('express/lib/response');
-
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
 });
 
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-    console.log("Database connected");
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Database connected');
 });
 
 const app = express();
 
-app.engine('ejs', ejsMate)
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 const sessionConfig = {
-    secret:'thisshouldbesecret!',
-    resave:false,
-    saveUninitialized: true,
-    cookie :{
-        httpOnly: true,
-        expires: Date.now() + 3600000*24*7,
-        maxAge : 3600000*24*7
-    }
-}
-app.use(session(sessionConfig))
+  secret: 'thisshouldbesecret!',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 3600000 * 24 * 7,
+    maxAge: 3600000 * 24 * 7,
+  },
+};
+app.use(session(sessionConfig));
 app.use(flash());
 
 app.use(passport.initialize());
@@ -61,49 +60,43 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
 app.use((req, res, next) => {
-    //console.log(req.session)
-    res.locals.currentUser = req.user;
-    res.locals.success = req.flash('success');
-    res.locals.error = req.flash('error');
-    next();
-})
+  // console.log(req.session)
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash('success');
+  res.locals.error = req.flash('error');
+  next();
+});
 
-
-/////////////////////////////////////////////////////
+/// //////////////////////////////////////////////////
 // app.get('/fakeUser', async (req, res) =>{
 //     const user = new User({email: 'abc@xyz.com', username : 'abc'})
 //     const newUser = await(User.register(user, 'chicken'))
 //     res.send(newUser);
 // })
-/////////////////////////////////////////////////////
+/// //////////////////////////////////////////////////
 
-app.use('/campgrounds', campgrounds)
-app.use('/campgrounds/:id/reviews', reviews)
-app.use('/',userRoutes)
+app.use('/campgrounds', campgrounds);
+app.use('/campgrounds/:id/reviews', reviews);
+app.use('/', userRoutes);
 
-
-//app.use(express.static('public'))
-app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('home')
+  res.render('home');
 });
 
-
-
-
 app.all('*', (req, res, next) => {
-    next(new ExpressError('Page Not Found', 404))
-})
+  next(new ExpressError('Page Not Found', 404));
+});
 
 app.use((err, req, res, next) => {
-    const { statusCode = 500 } = err;
-    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
-    res.status(statusCode).render('error', { err })
-})
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = 'Oh No, Something Went Wrong!';
+  res.status(statusCode).render('error', { err });
+});
 
 app.listen(3000, () => {
-    console.log('Serving on port 3000')
-})
+  console.log('Serving on port 3000');
+});
--- models/review.js
+++ models/review.js
@@ -1,12 +1,15 @@
-const { number } = require("joi");
-const mongoose = require("mongoose");
+const { number } = require('joi');
+const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema
+const { Schema } = mongoose;
 
 const reviewSchema = new Schema({
-    body : String,
-    rating : Number
+  body: String,
+  rating: Number,
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }
 });
 
-
-module.exports = mongoose.model("Review", reviewSchema);
+module.exports = mongoose.model('Review', reviewSchema);
